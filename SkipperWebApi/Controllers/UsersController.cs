a8a3fb4036b4b602623b6b22f85b023ade157b18
marcinreq <marcinreq@gmail.com>
2018-05-13 21:28:48 +0200

    Add users controller

---
+++ SkipperWebApi/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkipperWebApi.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "user1", "user2" };
+        }
+    }
+}
